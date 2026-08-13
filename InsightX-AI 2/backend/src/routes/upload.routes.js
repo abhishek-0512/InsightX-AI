4ae2396ea28ac9dba0808a856b36e3fa724fbbb7
Abhishek Gangwar <YOUR_GITHUB_EMAIL>
2026-08-14 01:27:09 +0530

    01 commit initial

--- InsightX-AI 2/backend/src/routes/upload.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const upload = require("../middleware/upload.middleware");
-const { uploadFile } = require("../controllers/upload.controller");
-
-router.post("/", upload.single("file"), uploadFile);
-
-module.exports = router;
